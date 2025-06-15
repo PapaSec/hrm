03a93dda91c5ee489faef2a8f5e2dd8990614481
PapaSec <fransronald@gmail.com>
2025-06-15 15:51:09 +0200

    Daily commit

--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Payroll extends Model
 {
-    //
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function salaries()
+    {
+        return $this->hasMany(Salary::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function scopeInCompany($query)
+    {
+        return $query->where('company_id', $this->company_id);
+    }
+
+    public function getMonthYearAttribute()
+    {
+        return $this->year . '-' . $this->month;
+    }
+
+    public function getMonthStringAttribute()
+    {
+        return Carbon::parse($this->month_year)->format('F Y');
+    }
 }
