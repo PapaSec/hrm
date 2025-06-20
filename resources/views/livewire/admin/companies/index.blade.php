85d9c8c09f8a65a19ecd505b3621fd01491a8402
PapaSec <fransronald@gmail.com>
2025-06-20 23:18:49 +0200

    Daily commits

--- resources/views/livewire/admin/companies/index.blade.php
+++ resources/views/livewire/admin/companies/index.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 dark:from-slate-900 dark:to-slate-800 p-6">
     <div class="relative mb-6 w-full">
         <flux:heading size="xl">Companies</flux:heading>
         <flux:subheading size="lg" class="mb-6">List of Companies</flux:subheading>
