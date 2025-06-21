0bd6e9ccd4222a4dcaeb592a30d1829d73866b00
PapaSec <fransronald@gmail.com>
2025-06-21 11:58:55 +0200

    Daily commits

--- resources/views/livewire/admin/companies/create.blade.php
+++ resources/views/livewire/admin/companies/create.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 dark:from-slate-900 dark:to-slate-800 p-6">
     <div class="relative mb-6 w-full">
         <flux:heading size="xl">Companies</flux:heading>
         <flux:subheading size="lg" class="mb-6">List of Companies</flux:subheading>
@@ -6,6 +6,11 @@
         <div>
 
             <form wire:submit="save" class="my-6 w-full space-y-6">
-                <flux:input label="Company Name" wire:model.live="company.name" :invalid="$errors" />
+                <flux:input label="Company Name" wire:model.live="company.name" :invalid="$errors->has('company.name')"
+                    type="text" />
+                <flux:input label="Email Address" wire:model.live="company.email"
+                    :invalid="$errors->has('company.email')" type="email" />
+                <flux:input label="Company Website" wire:model.live="company.website"
+                    :invalid="$errors->has('company.website')" type="url" />
             </form>
         </div>
